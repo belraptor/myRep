9af18f77a54dec370db38eff665dabd44ebecc0f
belra <belra@belraptor-pc>
2018-07-04 19:24:24 +0300

    1

---
+++ first/src/first/censor.java
@@ -0,0 +1,14 @@
+package first;
+
+import java.util.Scanner;
+
+public class censor {
+	//замена по тексту
+public static void main(String[] args) {
+	System.out.println("Введите фразу");
+	Scanner kbd = new Scanner(System.in);
+	String text = kbd.nextLine();
+	String text2 = text.replaceAll(",", " зпт ");
+	System.out.println(text2);
+}
+}
