e9dd58802cd1c9d062f330fbdc5f0e49cdeeb262
belra <belra@belraptor-pc>
2018-07-07 09:40:59 +0300

    1

--- first/src/first/censor.java
+++ first/src/first/censor.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
 	System.out.println("Введите фразу");
 	Scanner kbd = new Scanner(System.in);
 	String text = kbd.nextLine();
+	kbd.close();
 	String text2 = text.replaceAll(",", " зпт ");
 	System.out.println(text2);
 }
