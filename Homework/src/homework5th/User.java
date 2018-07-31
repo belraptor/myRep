bb9d40028506d6045cb1226dd1c58820b564100d
belra <belra@DESKTOP-HLU8BJ1>
2018-07-31 23:39:11 +0300

    1

--- Homework/src/homework5th/User.java
+++ Homework/src/homework5th/User.java
@@ -38,5 +38,37 @@ public class User {
 		month = 1+rand.nextInt(11);
 		dayOfMonth = 1+rand.nextInt(30);		
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (age != other.age)
+			return false;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 }
